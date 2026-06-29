531420cacc47ee48ea2ae8b822d76a8200f0e8b8
khg-android <noreply@localhost>
2026-06-29 11:56:08 -0400

    Bump version to 0.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.khg.android"
         minSdk = 23
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.6.1"
+        versionCode = 2
+        versionName = "0.7.0"
 
         ndk {
             abiFilters += listOf("armeabi-v7a")
